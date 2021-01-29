27f7a4972dfe709379bf24c6c2589cd549ab6f78
Mamadou <mamadou236@gmail.com>
2021-01-29 16:48:16 -0500

    Fixer les erreurs de lint

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,21 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './components/app/app.component';
-import {DrawingComponent} from './components/drawing/drawing.component';
-import {EditorComponent} from './components/editor/editor.component';
-import {MainPageComponent} from './components/main-page/main-page.component';
-import {SidebarComponent} from './components/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { DrawingComponent } from './components/drawing/drawing.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        EditorComponent,
-        SidebarComponent,
-        DrawingComponent,
-        MainPageComponent
-    ],
+    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
@@ -34,6 +28,6 @@ import {SidebarComponent} from './components/sidebar/sidebar.component';
         MatListModule,
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
