976f4e1dc173d35f5dee1bde37a970a2f66583e1
Gaya Mehenni <gaya.mehenni@polymtl.ca>
2021-02-01 11:13:01 -0500

    Debut outil line

--- client/src/app/services/tools/line-service.ts
+++ client/src/app/services/tools/line-service.ts
@@ -1,17 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
+import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { MouseButton } from './pencil-service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LineService extends Tool {
+    private points: Vec2[];
+
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.shortCutKey = 'l';
     }
 
-    stopDrawing(): void {
-        // Clear
+    onMouseDown(event: MouseEvent): void {
+        if (!(this.mouseDown = event.button === MouseButton.Left)) {
+            return;
+        }
+
+        if (this.points.length == 0) {
+            this.points.push(this.getPositionFromMouse(event));
+        }
+    }
+
+    onMouseUp(event: MouseEvent): void {
+        //
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        //
+    }
+
+    onMouseLeave(event: MouseEvent): void {
+        //
+    }
+
+    onMouseEnter(event: MouseEvent): void {
+        //
+    }
+
+    onKeyPress(event: KeyboardEvent): void {
+        //
+    }
+
+    stopDrawing(): void {}
+
+    getPositionFromMouse(event: MouseEvent): Vec2 {
+        return { x: event.offsetX, y: event.offsetY };
     }
 }
