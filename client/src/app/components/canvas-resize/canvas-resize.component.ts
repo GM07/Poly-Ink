9c48659dd862481e164024a173f0099411d251a3
Gaya Mehenni <gaya.mehenni@polymtl.ca>
2021-02-05 17:15:34 -0500

    Merge conflits part 2

--- client/src/app/components/canvas-resize/canvas-resize.component.ts
+++ client/src/app/components/canvas-resize/canvas-resize.component.ts
@@ -112,15 +112,15 @@ export class CanvasResizeComponent implements AfterViewInit {
             'margin-left': String(this.canvasLeft) + 'px',
             'margin-top': String(this.canvasTop) + 'px',
             // tslint:disable:prettier
-            'width': String(this.drawingService.canvas.width) + 'px',
-            'height': String(this.drawingService.canvas.height) + 'px',
+            width: String(this.drawingService.canvas.width) + 'px',
+            height: String(this.drawingService.canvas.height) + 'px',
             // tslint:enable:prettier
         };
         this.workZoneStyle = {
-          // tslint:disable:prettier
-           width: String(this.drawingService.canvas.width + CanvasConst.WORKING_SIZE) + 'px',
-           height: String(this.drawingService.canvas.height + CanvasConst.WORKING_SIZE) + 'px',
-           // tslint:enable:prettier
+            // tslint:disable:prettier
+            width: String(this.drawingService.canvas.width + CanvasConst.WORKING_SIZE) + 'px',
+            height: String(this.drawingService.canvas.height + CanvasConst.WORKING_SIZE) + 'px',
+            // tslint:enable:prettier
         };
     }
 
