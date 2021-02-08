178b34f43f3db44479c07cd392f7fc5428be1241
Mamadou <mamadou236@gmail.com>
2021-02-08 16:42:12 -0500

    Fin du hotfix

--- client/src/app/classes/tool_settings/index-bottom.ts
+++ client/src/app/classes/tool_settings/index-bottom.ts
@@ -1,7 +1,8 @@
 // Order matters here
 // Tool icons at the bottom of the side bar
-import { ExportSettings } from './export-settings';
-import { SaveSettings } from './save-settings';
+// import { ExportSettings } from './export-settings';
+import { NewDrawing } from './new-drawing-settings';
+// import { SaveSettings } from './save-settings';
 import { ToolSettings } from './tool-settings';
 
-export const BOTTOM_TOOLS: ToolSettings[] = [new SaveSettings(), new ExportSettings()];
+export const BOTTOM_TOOLS: ToolSettings[] = [new NewDrawing()];
