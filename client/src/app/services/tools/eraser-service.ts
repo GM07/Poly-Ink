a5b8908c46b2c207d6acd6076edfb5f4c2162079
Paul-André Bisson <>
2021-02-13 09:33:49 -0500

    modification de l'affichage css

--- client/src/app/services/tools/eraser-service.ts
+++ client/src/app/services/tools/eraser-service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
+import { EraserToolConstants } from '@app/classes/tool_settings/tools.constants';
 import { Vec2 } from '@app/classes/vec2';
 import { MouseButton } from '@app/constants/control.ts';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -21,6 +22,7 @@ export class EraserService extends Tool {
         super(drawingService, colorService);
         this.clearPath();
         this.shortCutKey = 'e';
+        this.toolID = EraserToolConstants.TOOL_ID;
     }
 
     get lineWidth(): number {
