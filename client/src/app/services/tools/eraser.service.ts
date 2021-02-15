0eb44385b3a070916a804c43e1a5261e1d673b62
Mamadou <mamadou236@gmail.com>
2021-02-15 10:26:01 -0500

    Fix des erreurs de name

--- client/src/app/services/tools/eraser.service.ts
+++ client/src/app/services/tools/eraser.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
-import { EraserToolConstants } from '@app/classes/tool_settings/tools.constants';
+import { EraserToolConstants } from '@app/classes/tool_ui_settings/tools.constants';
 import { Vec2 } from '@app/classes/vec2';
 import { MouseButton } from '@app/constants/control.ts';
 import { DrawingService } from '@app/services/drawing/drawing.service';
