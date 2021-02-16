b43e6638f8accba058cf78fcf88dafb143ff879f
Félix-Antoine Constantin <=>
2021-02-16 18:42:41 -0500

    Résolution du lint

--- client/src/app/services/drawing/canvas-reset.service.ts
+++ client/src/app/services/drawing/canvas-reset.service.ts
@@ -38,7 +38,7 @@ export class NewDrawingService {
     }
 
     isNotEmpty(baseCtx: CanvasRenderingContext2D, width: number, height: number): boolean {
-        const whiteColor = 4294967295; //Constante pour la couleur blanche
+        const whiteColor = 4294967295; // Constante pour la couleur blanche
         const pixelBuffer = new Uint32Array(baseCtx.getImageData(0, 0, width, height).data.buffer);
         return pixelBuffer.some((color) => color !== whiteColor) && pixelBuffer.some((color) => color !== 0);
     }
