54829c0e7de9fb5b82f094738e639fc28394759b
Paul-André Bisson <>
2021-02-15 21:15:57 -0500

    Fix de la dépendance circulaire et de la propagation des evenements

--- client/src/app/components/canvas-reset/canvas-reset.component.spec.ts
+++ client/src/app/components/canvas-reset/canvas-reset.component.spec.ts
@@ -1,8 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NewDrawingComponent } from './canvas-reset.component';
-
 import { NewDrawingService } from '@app/services/drawing/canvas-reset.service';
+import { NewDrawingComponent } from './canvas-reset.component';
 
 describe('NewDrawingComponent', () => {
     let component: NewDrawingComponent;
@@ -40,21 +38,29 @@ describe('NewDrawingComponent', () => {
     it('Should create a new drawing with ctrl+o', () => {
         const keyEvent = new KeyboardEvent('document:keydown', { ctrlKey: true, key: 'o' });
         const keySpy = spyOn(newDrawingService, 'newCanvas');
-        component.onKeyPressed(keyEvent);
+        component.onKeyDown(keyEvent);
         expect(keySpy).toHaveBeenCalled();
     });
 
     it('should not create a new drawing if not ctrl', () => {
         const keyEvent = new KeyboardEvent('document:keydown', { ctrlKey: false, key: 'o' });
         const keySpy = spyOn(newDrawingService, 'newCanvas');
-        component.onKeyPressed(keyEvent);
+        component.onKeyDown(keyEvent);
         expect(keySpy).not.toHaveBeenCalled();
     });
 
     it('should not create a new drawing with other key', () => {
         const keyEvent = new KeyboardEvent('document:keydown', { ctrlKey: true, key: 'b' });
         const keySpy = spyOn(newDrawingService, 'newCanvas');
-        component.onKeyPressed(keyEvent);
+        component.onKeyDown(keyEvent);
         expect(keySpy).not.toHaveBeenCalled();
+    });
+
+    it('should stop propagations if the warning is displayed', () => {
+        newDrawingService.showWarning = true;
+        const keyEvent = new KeyboardEvent('document:keydown', { ctrlKey: false, key: 'o' });
+        const propagation = spyOn(keyEvent, 'stopImmediatePropagation');
+        component.onKeyDown(keyEvent);
+        expect(propagation).toHaveBeenCalled();
     });
 });
