8ebc126f28a4d8a6c9385448b00cfab2049c1cd1
Paul-André Bisson <>
2021-02-22 10:20:33 -0500

    Amelioration des tests

--- client/src/app/classes/shortcut-key.spec.ts
+++ client/src/app/classes/shortcut-key.spec.ts
@@ -1,24 +1,48 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ShortcutKey } from '@app/classes/shortcut-key';
 
 describe('ShortcutKey', () => {
-    const shortcutKey: ShortcutKey = new ShortcutKey('c', false, false, false);
+    let shortcutKey: ShortcutKey;
 
-    beforeEach(async(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({});
-    }));
+        shortcutKey = new ShortcutKey('c', false, false, false);
+    });
 
     it('should be created', () => {
         expect(shortcutKey).toBeTruthy();
     });
 
     it('checks if a key event equals to the shortcut', () => {
-        shortcutKey.key = 'e';
         let event: KeyboardEvent = { key: 'l' } as KeyboardEvent;
         expect(shortcutKey.equals(event)).toBeFalsy();
-        event = { key: 'e', ctrlKey: false, shiftKey: false, altKey: false } as KeyboardEvent;
+        event = { key: 'c', ctrlKey: false, shiftKey: false, altKey: false } as KeyboardEvent;
         expect(shortcutKey.equals(event)).toBeTruthy();
-        event = { key: 'e', ctrlKey: true } as KeyboardEvent;
+        event = { key: 'c', ctrlKey: true } as KeyboardEvent;
         expect(shortcutKey.equals(event)).toBeFalsy();
+    });
+
+    it('allows for shortcuts with ctrl', () => {
+        shortcutKey.ctrlKey = true;
+        let event: KeyboardEvent = { key: 'c', ctrlKey: false, shiftKey: false, altKey: false } as KeyboardEvent;
+        expect(shortcutKey.equals(event)).toBeFalsy();
+        event = { key: 'c', ctrlKey: true, shiftKey: false, altKey: false } as KeyboardEvent;
+        expect(shortcutKey.equals(event)).toBeTruthy();
+    });
+
+    it('allows for shortcuts with shift', () => {
+        shortcutKey.shiftKey = true;
+        let event: KeyboardEvent = { key: 'c', ctrlKey: false, shiftKey: false, altKey: false } as KeyboardEvent;
+        expect(shortcutKey.equals(event)).toBeFalsy();
+        event = { key: 'c', ctrlKey: false, shiftKey: true, altKey: false } as KeyboardEvent;
+        expect(shortcutKey.equals(event)).toBeTruthy();
+    });
+
+    it('allows for shortcuts with alt', () => {
+        shortcutKey.altKey = true;
+        let event: KeyboardEvent = { key: 'c', ctrlKey: false, shiftKey: false, altKey: false } as KeyboardEvent;
+        expect(shortcutKey.equals(event)).toBeFalsy();
+        event = { key: 'c', ctrlKey: false, shiftKey: false, altKey: true } as KeyboardEvent;
+        expect(shortcutKey.equals(event)).toBeTruthy();
     });
 });
