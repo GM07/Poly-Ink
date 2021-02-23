7079db36560fb6421ec6dac6d99092b383c5eec1
Félix-Antoine Constantin <=>
2021-02-22 19:11:24 -0500

    Modification du shortcut

--- client/src/app/services/tools/ellipse-selection.service.ts
+++ client/src/app/services/tools/ellipse-selection.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ShortcutKey } from '@app/classes/shortcut-key';
-import { Tool } from '@app/classes/tool';
 import { EllipseSelectionToolConstants } from '@app/classes/tool_ui_settings/tools.constants';
 import { ColorService } from 'src/color-picker/services/color.service';
 import { DrawingService } from '../drawing/drawing.service';
 import { RectangleSelectionService } from './rectangle-selection.service';
 import { Vec2 } from '@app/classes/vec2';
+import { ShortcutKey } from '@app/classes/shortcut-key';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +14,7 @@ export class EllipseSelectionService extends RectangleSelectionService {
 
   constructor(drawingService: DrawingService, colorService: ColorService) {
     super(drawingService, colorService);
-    this.shortcutKey = EllipseSelectionToolConstants.SHORTCUT_KEY;
+    this.shortcutKey = new ShortcutKey(EllipseSelectionToolConstants.SHORTCUT_KEY);
     this.toolID = EllipseSelectionToolConstants.TOOL_ID;
   }
 
