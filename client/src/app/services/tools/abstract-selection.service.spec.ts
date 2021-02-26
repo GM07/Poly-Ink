9eeb61b295e4c7332426001fd35f74eae24bfe08
Félix-Antoine Constantin <=>
2021-02-25 23:47:51 -0500

    Ajout de quelques tests a la selection

--- client/src/app/services/tools/abstract-selection.service.spec.ts
+++ client/src/app/services/tools/abstract-selection.service.spec.ts
@@ -1,16 +1,135 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Vec2 } from '@app/classes/vec2';
 import { AbstractSelectionService } from './abstract-selection.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 
-describe('AbsractSelectionService', () => {
+describe('AbstractSelectionService', () => {
+
     let service: AbstractSelectionService;
+    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
+    let canvasSelection: HTMLCanvasElement;
+
+    let mouseEvent = {
+      offsetX: 25,
+      offsetY: 25,
+      button: 0,
+  } as MouseEvent;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+      drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: DrawingService, useValue: drawServiceSpy },
+      ],
+      });
         service = TestBed.inject(AbstractSelectionService);
+        canvasSelection = document.createElement('canvas');
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    it('set ArrowKeyUp should update the keys when down', () =>{
+      let keyboardEventLeft = new KeyboardEvent('keydown', {key: 'arrowleft'});
+      let keyboardEventRight = new KeyboardEvent('keydown', {key: 'arrowright'});
+      let keyboardEventDown = new KeyboardEvent('keydown', {key: 'arrowdown'});
+      let keyboardEventUp = new KeyboardEvent('keydown', {key: 'arrowup'});
+      (service as any).setArrowKeyUp(keyboardEventLeft);
+      (service as any).setArrowKeyUp(keyboardEventRight);
+      (service as any).setArrowKeyUp(keyboardEventUp);
+      (service as any).setArrowKeyUp(keyboardEventDown);
+      expect((service as any).isLeftArrowDown).toBe(true);
+      expect((service as any).isRightArrowDown).toBe(true);
+      expect((service as any).isDownArrowDown).toBe(true);
+      expect((service as any).isUpArrowDown).toBe(true);
+    });
+
+    it('set ArrowKeyUp should update the keys when not down', () =>{
+      let keyboardEventLeft = new KeyboardEvent('keydown', {key: 'randomKey'});
+      (service as any).setArrowKeyUp(keyboardEventLeft);
+      expect((service as any).isLeftArrowDown).toBe(false);
+      expect((service as any).isRightArrowDown).toBe(false);
+      expect((service as any).isDownArrowDown).toBe(false);
+      expect((service as any).isUpArrowDown).toBe(false);
+    });
+
+    it('set arrowKeyDown should update the keys when up', () =>{
+      (service as any).isLeftArrowDown = true;
+      (service as any).isRightArrowDown = true;
+      (service as any).isUpArrowDown = true;
+      (service as any).isDownArrowDown = true;
+      let keyboardEventLeft = new KeyboardEvent('keyup', {key: 'arrowleft'});
+      let keyboardEventRight = new KeyboardEvent('keyup', {key: 'arrowright'});
+      let keyboardEventDown = new KeyboardEvent('keyup', {key: 'arrowdown'});
+      let keyboardEventUp = new KeyboardEvent('keyup', {key: 'arrowup'});
+      (service as any).setArrowKeyDown(keyboardEventLeft);
+      (service as any).setArrowKeyDown(keyboardEventRight);
+      (service as any).setArrowKeyDown(keyboardEventUp);
+      (service as any).setArrowKeyDown(keyboardEventDown);
+      expect((service as any).isLeftArrowDown).toBe(false);
+      expect((service as any).isRightArrowDown).toBe(false);
+      expect((service as any).isDownArrowDown).toBe(false);
+      expect((service as any).isUpArrowDown).toBe(false);
+    });
+
+    it('set ArrowKeyDown should update the keys when not up', () =>{
+      let keyboardEventLeft = new KeyboardEvent('keydown', {key: 'randomKey'});
+      (service as any).setArrowKeyDown(keyboardEventLeft);
+      expect((service as any).isLeftArrowDown).toBe(false);
+      expect((service as any).isRightArrowDown).toBe(false);
+      expect((service as any).isDownArrowDown).toBe(false);
+      expect((service as any).isUpArrowDown).toBe(false);
+    });
+
+    it('is in selection should return true if is in selection', () => {
+      (service as any).selectionCtx = canvasSelection.getContext('2d');
+      (service as any).selectionCoords = {x: 0, y: 0} as Vec2;
+      (service as any).width = 50;
+      (service as any).height = 50;
+      spyOn(service as any, 'getPositionFromMouse').and.returnValue({x:25, y:25} as Vec2);
+      spyOn(service as any, 'isInSelection').and.callThrough();
+      expect(service.isInSelection(mouseEvent)).toEqual(true);
+    });
+
+    it('getXArrow should return 1 if right Arrow is down', () => {
+      (service as any).isRightArrowDown = true;
+      expect((service as any).getXArrow()).toEqual(1);
+    });
+
+    it('getXArrow should return -1 if left Arrow is down', () => {
+      (service as any).isLeftArrowDown = true;
+      expect((service as any).getXArrow()).toEqual(-1);
+    });
+
+    it('getYArrow should return 1 if down Arrow is down', () => {
+      (service as any).isDownArrowDown = true;
+      expect((service as any).getYArrow()).toEqual(1);
+    });
+
+    it('getYArrow should return -1 if up Arrow is down', () => {
+      (service as any).isUpArrowDown = true;
+      expect((service as any).getYArrow()).toEqual(-1);
+    });
+
+    it('get translation should return the current translation', () => {
+      let mousePos = {x: 25, y: 25} as Vec2;
+      (service as any).translationOrigin = {x: 25, y: 25} as Vec2;
+      expect((service as any).getTranslation(mousePos)).toEqual({x: 0, y: 0} as Vec2);
+    });
+
+    it('on mouse down should do nothing with different click', () => {
+      spyOn(service, 'getPositionFromMouse');
+      const badMouseEvent = {button: 1} as MouseEvent;
+      service.onMouseDown(badMouseEvent);
+      expect(service.getPositionFromMouse).not.toHaveBeenCalled();
+    });
+
+    it('should change translationOrigin when mouseDown and inSelection', () => {
+      spyOn(service, 'isInSelection').and.returnValue(true);
+      let saveTranslation : Vec2 = (service as any).translationOrigin;
+      service.onMouseDown(mouseEvent);
+      expect((service as any).translationOrigin).not.toEqual(saveTranslation);
+    });
+
 });
--- client/src/app/services/tools/rectangle-selection.service.spec.ts
+++ client/src/app/services/tools/rectangle-selection.service.spec.ts
@@ -1,16 +1,207 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Vec2 } from '@app/classes/vec2';
 import { RectangleSelectionService } from './rectangle-selection.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+
 
 describe('RectangleSelectionService', () => {
     let service: RectangleSelectionService;
+    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
+    let canvasSelection: HTMLCanvasElement;
+
+    let mouseEvent = {
+      offsetX: 25,
+      offsetY: 25,
+      button: 0,
+  } as MouseEvent;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+      drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
+        TestBed.configureTestingModule({
+          providers: [
+            { provide: DrawingService, useValue: drawServiceSpy },
+        ],
+        });
         service = TestBed.inject(RectangleSelectionService);
+        canvasSelection = document.createElement('canvas');
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should draw set mouseDown and mouseUp to same coordinate on mouseDown outside of selection', () => {
+      spyOn(service as any, 'endSelection');
+      spyOn(service as any, 'drawPreviewSelection');
+      spyOn(service, 'isInSelection').and.returnValue(false);
+      service.onMouseDown(mouseEvent);
+      expect((service as any).mouseDownCoord).toEqual((service as any).mouseUpCoord);
+    });
+
+    it('on mouse up should set mousePosition and clear canvas if in canvas', () => {
+      (service as any).mouseDown = true;
+      spyOn(service, 'getPositionFromMouse');
+      spyOn(service, 'isInCanvas').and.returnValue(true);
+      spyOn(service as any, 'startSelection');
+      service.onMouseUp(mouseEvent);
+      expect(service.getPositionFromMouse).toHaveBeenCalled();
+      expect((service as any).startSelection).toHaveBeenCalled();
+    });
+
+    it('on mouse up should moveSelection if canvas is set', () => {
+      (service as any).mouseDown = true;
+      spyOn(service, 'isInCanvas').and.returnValue(false);
+      spyOn(service as any, 'getTranslation').and.returnValue({x: 0, y: 0} as Vec2);
+      spyOn(service as any, 'moveSelection');
+      (service as any).selectionCtx = canvasSelection.getContext('2d');
+      service.onMouseUp(mouseEvent);
+      expect((service as any).moveSelection).toHaveBeenCalled();
+    })
+
+    it('should draw preview on mouse move', () => {
+      (service as any).mouseDown = true;
+      spyOn((service as any), 'drawPreviewSelection');
+      service.onMouseMove(mouseEvent);
+      expect((service as any).drawPreviewSelection).toHaveBeenCalled();
+    });
+
+    it('should update the selection on mouse move if the selection is not null', () => {
+      (service as any).mouseDown = true;
+      (service as any).selectionCtx = canvasSelection.getContext('2d');
+      spyOn((service as any), 'updateSelection');
+      spyOn(service as any, 'getTranslation');
+      service.onMouseMove(mouseEvent);
+      expect((service as any).updateSelection).toHaveBeenCalled();
+    });
+
+    it('should do nothing on mouse leave/enter if selection is empty', () => {
+      spyOn(service as any, 'updateDrawingSelection');
+      service.onMouseLeave(mouseEvent);
+      service.onMouseEnter(mouseEvent);
+      expect((service as any).updateDrawingSelection).not.toHaveBeenCalled();
+    });
+
+    it('should update the selection on mouse leave if the selection is null', () => {
+      spyOn(service as any, 'updateDrawingSelection');
+      (service as any).mouseDown = true;
+      spyOn(service, 'isInCanvas').and.returnValue(true);
+      service.onMouseLeave(mouseEvent);
+      expect((service as any).updateDrawingSelection).toHaveBeenCalled();
+    });
+
+    it('should update the selection on mouse enter if the selection is null', () => {
+      spyOn(service as any, 'updateDrawingSelection');
+      (service as any).mouseDown = true;
+      spyOn(service, 'isInCanvas').and.returnValue(true);
+      service.onMouseEnter(mouseEvent);
+      expect((service as any).updateDrawingSelection).toHaveBeenCalled();
+    });
+
+    it('should stop drawing when the esc key is pressed', () => {
+      let keyboardEvent = {key: 'escape', ctrlKey: false, shiftKey: false, altKey: false} as KeyboardEvent;
+      spyOn(service, 'stopDrawing');
+      service.onKeyDown(keyboardEvent);
+      expect(service.stopDrawing).toHaveBeenCalled();
+    });
+
+    it('should select all when alt+a is pressed', () => {
+      let keyboardEvent = new KeyboardEvent('document:keydown', {key: 'a', ctrlKey: true, shiftKey: false, altKey: false});
+      spyOn(service, 'selectAll');
+      service.onKeyDown(keyboardEvent);
+      expect(service.selectAll).toHaveBeenCalled();
+    });
+
+    it('should update the rectangle on shift pressed', () => {
+      let keyboardEvent = {ctrlKey: false, shiftKey: true, altKey: false} as KeyboardEvent;
+      (service as any).mouseDown = true;
+      spyOn(service as any, 'updateDrawingSelection');
+      service.onKeyDown(keyboardEvent);
+      expect((service as any).updateDrawingSelection).toHaveBeenCalled();
+    });
+
+   /* it('it should move the selection when there is one and an arrow is pressed', () => {
+      let keyboardEvent = new KeyboardEvent('document:keydown', {key: 'arrowleft', ctrlKey: false, shiftKey: false, altKey: false});
+      spyOn(service as any, 'moveSelection');
+      (service as any).selectionCtx = canvasSelection.getContext('2d');
+      service.onKeyDown(keyboardEvent);
+      expect((service as any).moveSelection).toHaveBeenCalled();
+    });*/ //For some reason this test is breaking jasmine.
+
+    it('should update the selection when key up', () => {
+      let keyboardEvent = {ctrlKey: false, shiftKey: false, altKey: false} as KeyboardEvent;
+      (service as any).shiftPressed = true;
+      (service as any).mouseDown = true;
+      spyOn(service as any, 'updateDrawingSelection');
+      service.onKeyUp(keyboardEvent);
+      expect((service as any).updateDrawingSelection).toHaveBeenCalled();
+    });
+
+    it('should update arrowkey on key up', () => {
+      let keyboardEvent = {ctrlKey: false, shiftKey: false, altKey: false} as KeyboardEvent;
+      spyOn(service as any, 'setArrowKeyDown');
+      spyOn(window, 'clearInterval');
+      (service as any).selectionCtx = canvasSelection.getContext('2d');
+      service.onKeyUp(keyboardEvent);
+      expect(window.clearInterval).toHaveBeenCalled();
+    });
+
+    it('select all should set mouseDownCoord to upleft and mouseUp coord to bottom left', () => {
+      drawServiceSpy.canvas = document.createElement('canvas');
+      drawServiceSpy.canvas.width = 10;
+      drawServiceSpy.canvas.height = 10;
+      let mouseDown = {x: 0, y: 0} as Vec2;
+      let mouseUp = {x: drawServiceSpy.canvas.width, y: drawServiceSpy.canvas.height} as Vec2;
+      spyOn(service as any, 'stopDrawing');
+      spyOn(service as any, 'startSelection');
+      service.selectAll();
+      expect(mouseDown).toEqual((service as any).mouseDownCoord);
+      expect(mouseUp).toEqual((service as any).mouseUpCoord);
+    });
+
+    it('should call endSelection and clear the preview on stop drawing', () => {
+      spyOn(service as any, 'endSelection');
+      service.stopDrawing();
+      expect((service as any).endSelection).toHaveBeenCalled();
+      expect(drawServiceSpy.clearCanvas).toHaveBeenCalled();
+    });
+
+    it('moveSelection should update the selection', () => {
+      spyOn(service as any, 'updateSelection');
+      (service as any).selectionCoords = {x: 0, y: 0} as Vec2;
+      (service as any).moveSelection(1, 1);
+      expect((service as any).updateSelection).toHaveBeenCalled();
+      expect((service as any).selectionCoords).toEqual({x: 1, y: 1} as Vec2);
+    });
+
+    it('start selection should do nothing if width or height is not set', () => {
+      (service as any).width = 0;
+      spyOn((service as any), 'drawPreviewSelection');
+      (service as any).startSelection();
+      expect((service as any).drawPreviewSelection).not.toHaveBeenCalled();
+    })
+
+    it('start selection should init needed variables', () => {
+      (service as any).width = 100;
+      (service as any).height = 100;
+      (service as any).mouseDownCoord = {x: 25, y: 25} as Vec2;
+      (service as any).selectionCtx = canvasSelection.getContext('2d');
+      canvasSelection.width = 250;
+      canvasSelection.height = 250;
+      drawServiceSpy.previewCanvas = document.createElement('canvas');
+      drawServiceSpy.previewCtx = drawServiceSpy.previewCanvas.getContext('2d') as CanvasRenderingContext2D;
+      (service as any).SELECTION_DATA = canvasSelection;
+      spyOn((service as any), 'drawPreviewSelection');
+      spyOn(drawServiceSpy.previewCtx, 'drawImage');
+      spyOn((service as any).selectionCtx, 'drawImage');
+      (service as any).startSelection();
+      expect(drawServiceSpy.previewCtx.drawImage).toHaveBeenCalled();
+      expect((service as any).drawPreviewSelection).toHaveBeenCalled();
+    });
+
+    it('update drawing should clear canvas and draw new preview', () => {
+      spyOn(service as any, 'drawPreviewSelection');
+      (service as any).updateDrawingSelection();
+      expect(drawServiceSpy.clearCanvas).toHaveBeenCalled();
+      expect((service as any).drawPreviewSelection).toHaveBeenCalled();
     });
 });
