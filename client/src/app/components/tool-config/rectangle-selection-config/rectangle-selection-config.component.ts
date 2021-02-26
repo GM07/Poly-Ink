6be6893982416b17580f364a5a805bbc65309af5
Paul-André Bisson <>
2021-02-25 21:00:06 -0500

    Ajout du premier control point

--- client/src/app/components/tool-config/rectangle-selection-config/rectangle-selection-config.component.ts
+++ client/src/app/components/tool-config/rectangle-selection-config/rectangle-selection-config.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { ToolConfig } from '@app/classes/tool-config';
 import { MouseButton } from '@app/constants/control';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -9,20 +9,36 @@ import { RectangleSelectionService } from '@app/services/tools/rectangle-selecti
     templateUrl: './rectangle-selection-config.component.html',
     styleUrls: ['./rectangle-selection-config.component.scss'],
 })
-export class RectangleSelectionConfigComponent extends ToolConfig implements OnDestroy, OnInit {
+export class RectangleSelectionConfigComponent extends ToolConfig implements OnDestroy, OnInit, AfterViewInit {
     isOverSelection: boolean = false;
     mouseDown: boolean = false;
     previewCanvas: HTMLCanvasElement;
     private lastCursor: string;
+    private readonly CANVAS_COORD: DOMRect;
+
+    controlRightStyle: { [key: string]: string };
 
     constructor(public rectangleSelectionService: RectangleSelectionService, drawingService: DrawingService) {
         super();
         this.previewCanvas = drawingService.previewCanvas;
+        this.CANVAS_COORD = drawingService.canvas.getBoundingClientRect();
+    }
+
+    ngAfterViewInit(): void {
+        const currentPos = document.getElementById('rectangle-resize')?.getBoundingClientRect();
+        if (currentPos === undefined) return;
+
+        //Apparait exactement en haut à gauche du canvas
+        this.controlRightStyle = {
+            left: String(this.CANVAS_COORD.x - currentPos.x) + 'px',
+        };
     }
 
     ngOnInit(): void {
+        console.log('ini');
         this.lastCursor = this.previewCanvas.style.cursor;
     }
+
     ngOnDestroy(): void {
         if (this.previewCanvas !== undefined) this.previewCanvas.style.cursor = this.lastCursor;
     }
