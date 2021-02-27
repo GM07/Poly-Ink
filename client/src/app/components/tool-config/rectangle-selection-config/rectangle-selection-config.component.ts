5cce130461b04de219f313460272c679738c16f7
Paul-André Bisson <>
2021-02-26 20:52:28 -0500

    Fix du positionnement absolu des points de controle

--- client/src/app/components/tool-config/rectangle-selection-config/rectangle-selection-config.component.ts
+++ client/src/app/components/tool-config/rectangle-selection-config/rectangle-selection-config.component.ts
@@ -21,39 +21,40 @@ export class RectangleSelectionConfigComponent extends ToolConfig implements OnD
     @ViewChild('bottomMiddle', { static: false }) bottomMiddle: ElementRef<HTMLElement>;
     @ViewChild('bottomRight', { static: false }) bottomRight: ElementRef<HTMLElement>;
 
-    private readonly CANVAS_COORD: DOMRect;
-    private readonly PREVIEW_CANVAS: HTMLCanvasElement;
-    private controlPointList: Map<ElementRef<HTMLElement>, DOMRect>;
+    private readonly CONTROL_POSITION: number = 8;
+    private readonly BORDER: number = 2;
+    private readonly CANVAS_COORD: Vec2;
+
+    private controlPointList: Map<ElementRef<HTMLElement>, Vec2>;
     private lastCursor: string;
 
     isOverSelection: boolean = false;
     mouseDown: boolean = false;
     displayControlPoints = false;
 
-    constructor(public selectionService: RectangleSelectionService, drawingService: DrawingService) {
+    constructor(public selectionService: RectangleSelectionService, private drawingService: DrawingService) {
         super();
-        this.PREVIEW_CANVAS = drawingService.previewCanvas;
-        this.CANVAS_COORD = drawingService.canvas.getBoundingClientRect();
+        this.CANVAS_COORD = this.setOriginalPosition(drawingService.canvas.getBoundingClientRect());
     }
 
     ngAfterViewInit(): void {
-        this.lastCursor = this.PREVIEW_CANVAS.style.cursor;
+        this.lastCursor = this.drawingService.previewCanvas.style.cursor;
     }
 
     ngOnDestroy(): void {
-        this.PREVIEW_CANVAS.style.cursor = this.lastCursor;
+        this.drawingService.previewCanvas.style.cursor = this.lastCursor;
     }
 
     initPoints() {
         this.controlPointList = new Map([
-            [this.topLeft, this.topLeft.nativeElement.getBoundingClientRect()],
-            [this.topMiddle, this.topMiddle.nativeElement.getBoundingClientRect()],
-            [this.topRight, this.topRight.nativeElement.getBoundingClientRect()],
-            [this.middleLeft, this.middleLeft.nativeElement.getBoundingClientRect()],
-            [this.middleRight, this.middleRight.nativeElement.getBoundingClientRect()],
-            [this.bottomLeft, this.bottomLeft.nativeElement.getBoundingClientRect()],
-            [this.bottomMiddle, this.bottomMiddle.nativeElement.getBoundingClientRect()],
-            [this.bottomRight, this.bottomRight.nativeElement.getBoundingClientRect()],
+            [this.topLeft, this.setOriginalPosition(this.topLeft.nativeElement.getBoundingClientRect())],
+            [this.topMiddle, this.setOriginalPosition(this.topMiddle.nativeElement.getBoundingClientRect())],
+            [this.topRight, this.setOriginalPosition(this.topRight.nativeElement.getBoundingClientRect())],
+            [this.middleLeft, this.setOriginalPosition(this.middleLeft.nativeElement.getBoundingClientRect())],
+            [this.middleRight, this.setOriginalPosition(this.middleRight.nativeElement.getBoundingClientRect())],
+            [this.bottomLeft, this.setOriginalPosition(this.bottomLeft.nativeElement.getBoundingClientRect())],
+            [this.bottomMiddle, this.setOriginalPosition(this.bottomMiddle.nativeElement.getBoundingClientRect())],
+            [this.bottomRight, this.setOriginalPosition(this.bottomRight.nativeElement.getBoundingClientRect())],
         ]);
 
         this.placePoints();
@@ -69,24 +70,35 @@ export class RectangleSelectionConfigComponent extends ToolConfig implements OnD
         const width = Math.abs(this.selectionService.width);
         const height = Math.abs(this.selectionService.height);
 
-        this.topLeft.nativeElement.style.left = String(this.getCanvasCoord(this.topLeft).x + x) + 'px';
-        this.topLeft.nativeElement.style.top = String(this.getCanvasCoord(this.topLeft).y + y) + 'px';
+        this.topLeft.nativeElement.style.left = String(this.getCanvasCoord(this.topLeft).x + x - this.CONTROL_POSITION) + 'px';
+        this.topLeft.nativeElement.style.top = String(this.getCanvasCoord(this.topLeft).y + y - this.CONTROL_POSITION) + 'px';
         this.topMiddle.nativeElement.style.left = String(this.getCanvasCoord(this.topMiddle).x + x + width / 2) + 'px';
-        this.topMiddle.nativeElement.style.top = String(this.getCanvasCoord(this.topMiddle).y + y) + 'px';
-        this.topRight.nativeElement.style.left = String(this.getCanvasCoord(this.topRight).x + x + width) + 'px';
-        this.topRight.nativeElement.style.top = String(this.getCanvasCoord(this.topRight).y + y) + 'px';
+        this.topMiddle.nativeElement.style.top = String(this.getCanvasCoord(this.topMiddle).y + y - this.CONTROL_POSITION) + 'px';
+        this.topRight.nativeElement.style.left = String(this.getCanvasCoord(this.topRight).x + x + width + this.BORDER) + 'px';
+        this.topRight.nativeElement.style.top = String(this.getCanvasCoord(this.topRight).y + y - this.CONTROL_POSITION) + 'px';
 
-        this.middleLeft.nativeElement.style.left = String(this.getCanvasCoord(this.middleLeft).x + x) + 'px';
+        this.middleLeft.nativeElement.style.left = String(this.getCanvasCoord(this.middleLeft).x + x - this.CONTROL_POSITION) + 'px';
         this.middleLeft.nativeElement.style.top = String(this.getCanvasCoord(this.middleLeft).y + y + height / 2) + 'px';
-        this.middleRight.nativeElement.style.left = String(this.getCanvasCoord(this.middleRight).x + x + width) + 'px';
+        this.middleRight.nativeElement.style.left = String(this.getCanvasCoord(this.middleRight).x + x + width + this.BORDER) + 'px';
         this.middleRight.nativeElement.style.top = String(this.getCanvasCoord(this.middleRight).y + y + height / 2) + 'px';
 
-        this.bottomLeft.nativeElement.style.left = String(this.getCanvasCoord(this.bottomLeft).x + x) + 'px';
-        this.bottomLeft.nativeElement.style.top = String(this.getCanvasCoord(this.bottomLeft).y + y + height) + 'px';
+        this.bottomLeft.nativeElement.style.left = String(this.getCanvasCoord(this.bottomLeft).x + x - this.CONTROL_POSITION) + 'px';
+        this.bottomLeft.nativeElement.style.top = String(this.getCanvasCoord(this.bottomLeft).y + y + height + this.BORDER) + 'px';
         this.bottomMiddle.nativeElement.style.left = String(this.getCanvasCoord(this.bottomMiddle).x + x + width / 2) + 'px';
-        this.bottomMiddle.nativeElement.style.top = String(this.getCanvasCoord(this.bottomMiddle).y + y + height) + 'px';
-        this.bottomRight.nativeElement.style.left = String(this.getCanvasCoord(this.bottomRight).x + x + width) + 'px';
-        this.bottomRight.nativeElement.style.top = String(this.getCanvasCoord(this.bottomRight).y + y + height) + 'px';
+        this.bottomMiddle.nativeElement.style.top = String(this.getCanvasCoord(this.bottomMiddle).y + y + height + this.BORDER) + 'px';
+        this.bottomRight.nativeElement.style.left = String(this.getCanvasCoord(this.bottomRight).x + x + width + this.BORDER) + 'px';
+        this.bottomRight.nativeElement.style.top = String(this.getCanvasCoord(this.bottomRight).y + y + height + this.BORDER) + 'px';
+
+        for (const [elementRef, domRect] of this.controlPointList) {
+            const pos = elementRef.nativeElement.getBoundingClientRect();
+            if (this.isInCanvas({ x: pos.x, y: pos.y } as Vec2)) {
+                elementRef.nativeElement.style.opacity = '1';
+                elementRef.nativeElement.style.pointerEvents = 'auto';
+            } else {
+                elementRef.nativeElement.style.opacity = '0';
+                elementRef.nativeElement.style.pointerEvents = 'none';
+            }
+        }
     }
 
     onMouseDown(event: MouseEvent) {
@@ -106,9 +118,9 @@ export class RectangleSelectionConfigComponent extends ToolConfig implements OnD
         if (!this.mouseDown) {
             this.isOverSelection = this.selectionService.isInSelection(event);
             if (this.isOverSelection) {
-                this.PREVIEW_CANVAS.style.cursor = 'all-scroll';
+                this.drawingService.previewCanvas.style.cursor = 'all-scroll';
             } else {
-                this.PREVIEW_CANVAS.style.cursor = this.lastCursor;
+                this.drawingService.previewCanvas.style.cursor = this.lastCursor;
             }
         } else {
             if (this.displayControlPoints) {
@@ -117,11 +129,27 @@ export class RectangleSelectionConfigComponent extends ToolConfig implements OnD
         }
     }
 
+    private isInCanvas(position: Vec2): boolean {
+        const clientRect = this.drawingService.canvas.getBoundingClientRect();
+        const left = clientRect.x;
+        const right = clientRect.x + clientRect.width;
+        const top = clientRect.y;
+        const bottom = clientRect.y + clientRect.height;
+        if (position.x <= left || position.x >= right || position.y <= top || position.y >= bottom) return false;
+        return true;
+    }
+
     private getCanvasCoord(control: ElementRef<HTMLElement>): Vec2 {
         const pos = this.controlPointList.get(control);
         if (pos === undefined) return { x: 0, y: 0 } as Vec2;
         const left = this.CANVAS_COORD.x - pos.x;
         const top = this.CANVAS_COORD.y - pos.y;
         return { x: left, y: top } as Vec2;
+    }
+
+    // Il faut prendre en compte l'état de fenêtre window.scroll,
+    // sinon lorsqu'il y a une barre de défilement, la position des points devient erronée
+    private setOriginalPosition(domRect: DOMRect): Vec2 {
+        return { x: domRect.x + window.scrollX, y: domRect.y + window.scrollY } as Vec2;
     }
 }
