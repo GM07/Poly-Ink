0d0989db0277ed9f34c11daff5dfff61e9435e71
Paul-André Bisson <>
2021-02-28 16:42:18 -0500

    Ajout de tests pour la selection abstraite

--- client/src/app/components/tool-config/rectangle-selection-config/rectangle-selection-config.component.spec.ts
+++ client/src/app/components/tool-config/rectangle-selection-config/rectangle-selection-config.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, /*ComponentFixture,*/ TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RectangleSelectionService } from '@app/services/tools/rectangle-selection.service';
 import { RectangleSelectionConfigComponent } from './rectangle-selection-config.component';
 
 describe('RectangleSelectionConfigComponent', () => {
-    //let component: RectangleSelectionConfigComponent;
-    //let fixture: ComponentFixture<RectangleSelectionConfigComponent>;
+    let component: RectangleSelectionConfigComponent;
+    let fixture: ComponentFixture<RectangleSelectionConfigComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [RectangleSelectionConfigComponent],
+            providers: [{ provide: RectangleSelectionService }],
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        //fixture = TestBed.createComponent(RectangleSelectionConfigComponent);
-        //component = fixture.componentInstance;
-        //fixture.detectChanges();
+        fixture = TestBed.createComponent(RectangleSelectionConfigComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
 
     it('should create', () => {
-      expect(true).toBe(true);
-      // expect(component).toBeTruthy();
+        expect(component).toBeTruthy();
     });
 });
