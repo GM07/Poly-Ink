b7344f36ff6528e40ee2bf06770d7ad26eda3adf
dentalfloss <florence.cloutier@polymtl.ca>
2021-02-28 16:13:14 -0500

    2*rayon devient juste diametre

--- client/src/app/services/tools/aerosol.service.ts
+++ client/src/app/services/tools/aerosol.service.ts
@@ -133,9 +133,9 @@ export class AerosolService extends Tool {
 
     randomDroplet(): Vec2 {
         const areaRadius = this.areaDiameter / 2;
-        const angle = 2 * Math.PI * (Math.random() * areaRadius);
+        const angle = Math.PI * Math.random() * this.areaDiameter;
         const randomDistFromCenter = Math.random() * areaRadius + Math.random() * areaRadius;
-        const randomRadius: number = randomDistFromCenter > areaRadius ? this.areaDiameter - randomDistFromCenter : randomDistFromCenter;
+        const randomRadius = randomDistFromCenter > areaRadius ? this.areaDiameter - randomDistFromCenter : randomDistFromCenter;
         return {
             x: Math.cos(angle) * randomRadius,
             y: Math.sin(angle) * randomRadius,
