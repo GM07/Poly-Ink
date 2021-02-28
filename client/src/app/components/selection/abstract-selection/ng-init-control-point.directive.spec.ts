81c487a2bf5d1e33835359a51c57a6d592ec6390
Paul-André Bisson <>
2021-02-28 16:12:32 -0500

    Ajout de test pour la directive des points de controle

--- client/src/app/components/selection/abstract-selection/ng-init-control-point.directive.spec.ts
+++ client/src/app/components/selection/abstract-selection/ng-init-control-point.directive.spec.ts
@@ -1,8 +1,22 @@
 import { NgInitControlPointDirective } from './ng-init-control-point.directive';
 
 describe('NgInitControlPointDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgInitControlPointDirective();
-    expect(directive).toBeTruthy();
-  });
+    let directive: NgInitControlPointDirective;
+
+    const delay = async (ms: number) => new Promise((result) => setTimeout(result, ms));
+    beforeEach(() => {
+        directive = new NgInitControlPointDirective();
+    });
+
+    it('should create an instance', () => {
+        const directive = new NgInitControlPointDirective();
+        expect(directive).toBeTruthy();
+    });
+
+    it('should emit an event when initialised', async () => {
+        spyOn(directive.initEvent, 'emit');
+        directive.ngOnInit();
+        await delay(11);
+        expect(directive.initEvent.emit).toHaveBeenCalledTimes(1);
+    });
 });
