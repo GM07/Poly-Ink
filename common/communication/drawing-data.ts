465b133ba999de602442e989a8c80f51b4c51f0e
Gaya Mehenni <gaya.mehenni@polymtl.ca>
2021-03-03 15:53:19 -0500

    Separation drawing et drawing data

--- common/communication/drawing-data.ts
+++ common/communication/drawing-data.ts
@@ -1,15 +1,15 @@
 import { Tag } from '@common/communication/tag';
 
 export class DrawingData {
-    title: string;
+    name: string;
     tags: Tag[];
 
-    constructor(title: string, tags: Tag[] = []) {
-        this.title = title;
+    constructor(name: string, tags: Tag[] = []) {
+        this.name = name;
         this.tags = tags;
     }
 
     static fromAny(object: any): DrawingData {
-        return new DrawingData(object.title, object.tags);
+        return new DrawingData(object.name, object.tags);
     }
 }
