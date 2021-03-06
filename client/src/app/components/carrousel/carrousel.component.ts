c1405f6d9b218ab8e964263c507835e98e90f02d
Paul-André Bisson <>
2021-03-05 22:06:43 -0500

    Modification de l'affichage pour des fiches

--- client/src/app/components/carrousel/carrousel.component.ts
+++ client/src/app/components/carrousel/carrousel.component.ts
@@ -13,13 +13,13 @@ import { ShortcutHandlerService } from '@app/services/shortcut/shortcut-handler.
             state(
                 'left',
                 style({
-                    transform: 'translateX(-150px)',
+                    transform: 'translateX(-250px)',
                 }),
             ),
             state(
                 'right',
                 style({
-                    transform: 'translateX(150px)',
+                    transform: 'translateX(250px)',
                 }),
             ),
             state(
@@ -38,6 +38,7 @@ export class CarrouselComponent implements AfterViewInit {
     @ViewChild('overflowLeftPreview', { static: false }) overflowLeftPreview: ElementRef<HTMLCanvasElement>;
     @ViewChild('leftPreview', { static: false }) leftPreview: ElementRef<HTMLCanvasElement>;
     @ViewChild('middlePreview', { static: false }) middlePreview: ElementRef<HTMLCanvasElement>;
+    @ViewChild('middlePreviewFiche') middlePreviewFiche: ElementRef<HTMLElement>;
     @ViewChild('rightPreview', { static: false }) rightPreview: ElementRef<HTMLCanvasElement>;
     @ViewChild('overflowRightPreview', { static: false }) overflowRightPreview: ElementRef<HTMLCanvasElement>;
 
@@ -70,65 +71,65 @@ export class CarrouselComponent implements AfterViewInit {
     // TODO: Va être remplacé par la requète de dessins sauvegardés
     // Ceci n'est utile que pour tester le fonctionnement.
     initTest(): void {
-        this.leftPreview.nativeElement.width = 100;
-        this.leftPreview.nativeElement.height = 100;
+        this.leftPreview.nativeElement.width = 200;
+        this.leftPreview.nativeElement.height = 200;
 
-        this.middlePreview.nativeElement.width = 100;
-        this.middlePreview.nativeElement.height = 100;
+        this.middlePreview.nativeElement.width = 200;
+        this.middlePreview.nativeElement.height = 200;
 
-        this.rightPreview.nativeElement.width = 100;
-        this.rightPreview.nativeElement.height = 100;
+        this.rightPreview.nativeElement.width = 200;
+        this.rightPreview.nativeElement.height = 200;
 
-        this.overflowLeftPreview.nativeElement.width = 100;
-        this.overflowLeftPreview.nativeElement.height = 100;
+        this.overflowLeftPreview.nativeElement.width = 200;
+        this.overflowLeftPreview.nativeElement.height = 200;
 
-        this.overflowRightPreview.nativeElement.width = 100;
-        this.overflowRightPreview.nativeElement.height = 100;
+        this.overflowRightPreview.nativeElement.width = 200;
+        this.overflowRightPreview.nativeElement.height = 200;
 
         // c1 à c3 créés pour tester
         const c1 = document.createElement('canvas');
-        c1.width = 100;
-        c1.height = 100;
+        c1.width = 200;
+        c1.height = 200;
         const ctxc1 = c1.getContext('2d');
         if (ctxc1 !== null) {
             ctxc1.fillStyle = 'red';
-            ctxc1.fillRect(0, 0, 100, 100);
+            ctxc1.fillRect(0, 0, 200, 200);
         }
 
         const c2 = document.createElement('canvas');
-        c2.width = 100;
-        c2.height = 100;
+        c2.width = 200;
+        c2.height = 200;
         const ctxc2 = c2.getContext('2d');
         if (ctxc2 !== null) {
             ctxc2.fillStyle = 'green';
-            ctxc2.fillRect(0, 0, 100, 100);
+            ctxc2.fillRect(0, 0, 200, 200);
         }
 
         const c3 = document.createElement('canvas');
-        c3.width = 100;
-        c3.height = 100;
+        c3.width = 200;
+        c3.height = 200;
         const ctxc3 = c3.getContext('2d');
         if (ctxc3 !== null) {
             ctxc3.fillStyle = 'blue';
-            ctxc3.fillRect(0, 0, 100, 100);
+            ctxc3.fillRect(0, 0, 200, 200);
         }
 
         const c4 = document.createElement('canvas');
-        c4.width = 100;
-        c4.height = 100;
+        c4.width = 200;
+        c4.height = 200;
         const ctxc4 = c4.getContext('2d');
         if (ctxc4 !== null) {
             ctxc4.fillStyle = 'grey';
-            ctxc4.fillRect(0, 0, 100, 100);
+            ctxc4.fillRect(0, 0, 200, 200);
         }
 
         const c5 = document.createElement('canvas');
-        c5.width = 100;
-        c5.height = 100;
+        c5.width = 200;
+        c5.height = 200;
         const ctxc5 = c5.getContext('2d');
         if (ctxc5 !== null) {
             ctxc5.fillStyle = 'pink';
-            ctxc5.fillRect(0, 0, 100, 100);
+            ctxc5.fillRect(0, 0, 200, 200);
         }
 
         this.canvasList = [c1, c2, c3];
