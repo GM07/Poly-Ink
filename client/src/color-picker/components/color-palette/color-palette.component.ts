fb36dc54bb8d92135b63d6624cdf7f953a634958
Félix-Antoine Constantin <=>
2021-03-11 10:07:18 -0500

    Elimination de parametres inutiles dans le color picker

--- client/src/color-picker/components/color-palette/color-palette.component.ts
+++ client/src/color-picker/components/color-palette/color-palette.component.ts
@@ -111,7 +111,7 @@ export class ColorPaletteComponent implements AfterViewInit, OnDestroy {
     }
 
     @HostListener('window:mouseup', ['$event'])
-    onMouseUp(event: MouseEvent): void {
+    onMouseUp(): void {
         this.mouseDown = false;
     }
 
