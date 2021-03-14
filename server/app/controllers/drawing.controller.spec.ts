a44dbb0624f28153f7fb5921013fc127b8e51ff3
Mamadu Guennady Barri <mamadu-guennady.barri@polymtl.ca>
2021-03-14 14:55:27 -0400

    Debut des tests pour le controlleur principal

---
+++ server/app/controllers/drawing.controller.spec.ts
@@ -0,0 +1,63 @@
+import { Drawing } from '../../../common/communication/drawing';
+import { DrawingData } from '../../../common/communication/drawing-data';
+import { Tag } from '../../../common/communication/tag';
+import { Stubbed, testingContainer } from '../../test/test-utils';
+import { Application } from '../app';
+import { BASE64_IMG } from '../classes/drawings.const';
+import { DrawingService } from '../services/drawing.service';
+import { TYPES } from '../types';
+
+// tslint:disable:no-any
+// const HTTP_STATUS_OK = 200;
+// const HTTP_STATUS_CREATED = 201;
+
+
+describe('DrawingController', () => {
+    const baseTags = [{
+        name: 'tag1'
+    } as Tag, {
+        name: 'tag2'
+    } as Tag];
+    
+    const baseDrawingDatas = [{
+        "tags": [baseTags[0]],
+        "name": "Tag 1"
+    } as DrawingData, {
+        "tags": [baseTags[1]],
+        "name": "Tag 2"
+    }];
+    const baseDrawing = [{
+        "data": baseDrawingDatas[0],
+        "image": BASE64_IMG
+    } as Drawing, {
+        "data": baseDrawingDatas[1],
+        "image": BASE64_IMG,
+    }];
+
+    let drawingService: Stubbed<DrawingService>;
+    let app: Express.Application;
+
+    beforeEach(async () => {
+        const [container, sandbox] = await testingContainer();
+        container.rebind(TYPES.DrawingService).toConstantValue({
+            validateName: sandbox.stub().resolves(true),
+            validateTags: sandbox.stub().resolves(true),
+            storeDrawing: sandbox.stub().resolves(),
+            getLocalDrawing: sandbox.stub().resolves(BASE64_IMG),
+            getAllDrawingsData: sandbox.stub().resolves([baseDrawingDatas[0], baseDrawingDatas[1]]),
+            getDrawingsDataFromTags: sandbox.stub().resolves(baseDrawingDatas[0]),
+            getDrawingDataFromName: sandbox.stub().resolves(baseDrawingDatas[1]),
+            createNewDrawingData: sandbox.stub().resolves('id1'),
+            createNewDrawingDataFromName: sandbox.stub().resolves('id2'),
+            deleteDrawingDataFromId: sandbox.stub().resolves(),
+            deleteDrawingData: sandbox.stub().resolves(),
+        });
+
+        drawingService = container.get(TYPES.DrawingService);
+        app = container.get<Application>(TYPES.Application).app;
+    });
+
+    it('should return ', () => {
+        return true;
+    });
+});
