c11dd29e6fcf051f56c8c927222668dd9d0991eb
zedfly <alexis.foulon@hotmail.com>
2021-03-19 17:45:59 -0400

    undo-redo tool-ui-settings are readonly

--- client/src/app/classes/tool_ui_settings/undo-settings.ts
+++ client/src/app/classes/tool_ui_settings/undo-settings.ts
@@ -2,8 +2,8 @@ import { ToolSettings } from './tool-settings';
 import { UndoConstants } from './tools.constants';
 
 export class Undo implements ToolSettings {
-    toolId: string = UndoConstants.TOOL_ID;
-    toolTitle: string = UndoConstants.TOOL_TITLE;
-    infoBubble: string = UndoConstants.INFO_BUBBLE;
-    iconName: string = UndoConstants.ICON_NAME;
+    readonly toolId: string = UndoConstants.TOOL_ID;
+    readonly toolTitle: string = UndoConstants.TOOL_TITLE;
+    readonly infoBubble: string = UndoConstants.INFO_BUBBLE;
+    readonly iconName: string = UndoConstants.ICON_NAME;
 }
