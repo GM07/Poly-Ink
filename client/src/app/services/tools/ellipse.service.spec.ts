6fe5fd7540d5523ca9d1d53ce3ba7d4bf7eb38ee
Félix-Antoine Constantin <=>
2021-03-21 20:12:22 -0400

    Modification du polygone pour qu'il hérite de la classe abstraite shape

--- client/src/app/services/tools/ellipse.service.spec.ts
+++ client/src/app/services/tools/ellipse.service.spec.ts
@@ -113,20 +113,20 @@ describe('EllipseService', () => {
     });
 
     it('should update the rectangle when the mouse leaves', () => {
-        service.onMouseLeave();
+        service.onMouseLeave({} as MouseEvent);
         expect(drawPreviewSpy).not.toHaveBeenCalled();
         service.onMouseDown(mouseEvent);
         mouseEvent = { clientX: 1, clientY: 1, button: 0 } as MouseEvent;
-        service.onMouseLeave();
+        service.onMouseLeave({} as MouseEvent);
         expect(drawPreviewSpy).toHaveBeenCalled();
     });
 
     it('should update the rectangle when the mouse enters', () => {
-        service.onMouseEnter();
+        service.onMouseEnter({} as MouseEvent);
         expect(drawPreviewSpy).not.toHaveBeenCalled();
         service.onMouseDown(mouseEvent);
         mouseEvent = { clientX: 1, clientY: 1, button: 0 } as MouseEvent;
-        service.onMouseEnter();
+        service.onMouseEnter({} as MouseEvent);
         expect(drawPreviewSpy).toHaveBeenCalled();
     });
 
--- client/src/app/services/tools/polygone.service.ts
+++ client/src/app/services/tools/polygone.service.ts
@@ -1,34 +1,20 @@
 import { Injectable } from '@angular/core';
 import { PolygonDraw } from '@app/classes/commands/polygon-draw';
 import { ShortcutKey } from '@app/classes/shortcut/shortcut-key';
-import { Tool } from '@app/classes/tool';
-import { PolygonConfig } from '@app/classes/tool-config/polygon-config';
 import { PolygoneToolConstants } from '@app/classes/tool_ui_settings/tools.constants';
-import { MouseButton } from '@app/constants/control';
 import { ToolSettingsConst } from '@app/constants/tool-settings';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { AbstractShape } from '@app/services/tools/abstract-shape.service';
 import { ColorService } from 'src/color-picker/services/color.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class PolygoneService extends Tool {
-    toolID: string;
-    config: PolygonConfig;
-
+export class PolygoneService extends AbstractShape {
     constructor(drawingService: DrawingService, colorService: ColorService) {
         super(drawingService, colorService);
         this.shortcutKey = new ShortcutKey(PolygoneToolConstants.SHORTCUT_KEY);
         this.toolID = PolygoneToolConstants.TOOL_ID;
-        this.config = new PolygonConfig();
-    }
-
-    set contourWidth(width: number) {
-        this.config.lineWidth = Math.min(Math.max(width, 1), ToolSettingsConst.MAX_WIDTH);
-    }
-
-    get contourWidth(): number {
-        return this.config.lineWidth;
     }
 
     set numEdges(numEdges: number) {
@@ -39,52 +25,6 @@ export class PolygoneService extends Tool {
 
     get numEdges(): number {
         return this.config.numEdges;
-    }
-
-    stopDrawing(): void {
-        this.leftMouseDown = false;
-        this.drawingService.clearCanvas(this.drawingService.previewCtx);
-    }
-
-    onMouseDown(event: MouseEvent): void {
-        this.leftMouseDown = event.button === MouseButton.Left;
-        if (this.leftMouseDown) {
-            this.config.startCoords = this.getPositionFromMouse(event);
-            this.config.endCoords = this.config.startCoords;
-            this.drawPreview();
-        }
-    }
-
-    onMouseUp(event: MouseEvent): void {
-        if (this.leftMouseDown) {
-            if (this.isInCanvas(event)) {
-                this.config.endCoords = this.getPositionFromMouse(event);
-            }
-            this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            this.draw();
-        }
-        this.leftMouseDown = false;
-    }
-
-    onMouseMove(event: MouseEvent): void {
-        if (this.leftMouseDown) {
-            this.config.endCoords = this.getPositionFromMouse(event);
-            this.drawPreview();
-        }
-    }
-
-    onMouseLeave(event: MouseEvent): void {
-        if (this.leftMouseDown) {
-            this.config.endCoords = this.getPositionFromMouse(event);
-            this.drawPreview();
-        }
-    }
-
-    onMouseEnter(event: MouseEvent): void {
-        if (this.leftMouseDown) {
-            this.config.endCoords = this.getPositionFromMouse(event);
-            this.drawPreview();
-        }
     }
 
     draw(): void {
