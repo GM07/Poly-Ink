42d079b520492039b28e3dba91454d46307810b6
dentalfloss <florence.cloutier@polymtl.ca>
2021-04-09 08:01:53 -0400

    Ajout subscription

--- client/src/app/services/tools/text.service.ts
+++ client/src/app/services/tools/text.service.ts
@@ -4,6 +4,7 @@ import { ShortcutKey } from '@app/classes/shortcut/shortcut-key';
 import { Tool } from '@app/classes/tool';
 import { TextConfig } from '@app/classes/tool-config/text-config';
 import { TextToolConstants } from '@app/classes/tool_ui_settings/tools.constants';
+import { Subject } from 'rxjs';
 import { ColorService } from 'src/color-picker/services/color.service';
 import { DrawingService } from '../drawing/drawing.service';
 
@@ -21,6 +22,8 @@ export class TextService extends Tool {
   arrowDown: ShortcutKey = new ShortcutKey('arrowdown');
   shortcutList: ShortcutKey[] = [this.delete, this.backspace, this.escape, this.arrowLeft, this.arrowRight, this.arrowUp, this.arrowDown];
 
+  handleIconClicked: Subject<boolean>;
+
   constructor(public drawingService: DrawingService, public colorService: ColorService) {
     super(drawingService, colorService);
 
@@ -28,6 +31,7 @@ export class TextService extends Tool {
     this.toolID = TextToolConstants.TOOL_ID;
 
     this.config = new TextConfig();
+    this.handleIconClicked = new Subject<boolean>();
   }
   
   onKeyDown(event: KeyboardEvent): void {
--- client/src/app/text/text.component.ts
+++ client/src/app/text/text.component.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ShortcutKey } from '@app/classes/shortcut/shortcut-key';
 import { MouseButton } from '@app/constants/control';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ShortcutHandlerService } from '@app/services/shortcut/shortcut-handler.service';
 import { TextService } from '@app/services/tools/text.service';
+import { Subscription } from 'rxjs';
 import { ColorService } from 'src/color-picker/services/color.service';
 
 @Component({
@@ -11,16 +12,21 @@ import { ColorService } from 'src/color-picker/services/color.service';
     templateUrl: './text.component.html',
     styleUrls: ['./text.component.scss'],
 })
-export class TextComponent {
+export class TextComponent implements OnInit {
   private leftMouseDown: boolean;
   private readonly ESCAPE: ShortcutKey = new ShortcutKey('escape');
-  //private currentIndex: number;
+  public subscriptionIconClick: Subscription;
 
   @ViewChild('textBox', { static: false }) textBox: ElementRef<HTMLTextAreaElement>;
 
   constructor(public textService: TextService, public shortcutHandlerService: ShortcutHandlerService, public drawingService: DrawingService, public colorService: ColorService) {
     this.ESCAPE.isDown = false;
-    //this.currentIndex = 0;
+  }
+
+  ngOnInit() {
+    this.subscriptionIconClick = this.textService.handleIconClicked.subscribe(() => {
+      this.confirmText();
+    });
   }
 
   onMouseDown(event: MouseEvent): void {
