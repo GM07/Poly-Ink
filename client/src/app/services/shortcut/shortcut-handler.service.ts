bea14e7427ddb6d1abde68d9b52d56ad99fb7d0e
Félix-Antoine Constantin <felix-antoine.constantin@polymtl.ca>
2021-04-12 11:06:04 -0400

    Le magnetism est bloqué lorsque les shortcuts sont bloques

--- client/src/app/services/shortcut/shortcut-handler.service.ts
+++ client/src/app/services/shortcut/shortcut-handler.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ClipboardService } from '@app/services/clipboard/clipboard.service';
-import { GridService } from '@app/services/drawing/grid.service';
+import { MagnetismService } from '@app/services/drawing/magnetism.service';
 import { TextService } from '@app/services/tools/text.service';
 import { ToolHandlerService } from '@app/services/tools/tool-handler.service';
 import { UndoRedoService } from '@app/services/undo-redo/undo-redo.service';
@@ -17,8 +17,8 @@ export class ShortcutHandlerService {
     constructor(
         private toolHandlerService: ToolHandlerService,
         private undoRedoService: UndoRedoService,
-        private gridService: GridService,
         private clipboardService: ClipboardService,
+        private magnetismService: MagnetismService,
     ) {
         this.blockShortcutsIn = false;
         this.blockShortcutsEvent = new Subject<boolean>();
@@ -43,7 +43,8 @@ export class ShortcutHandlerService {
             this.undoRedoService.onKeyDown(event);
             this.toolHandlerService.onKeyDown(event);
             this.clipboardService.onKeyDown(event);
-            this.gridService.onKeyDown(event);
+            this.magnetismService.gridService.onKeyDown(event);
+            this.magnetismService.onKeyDown(event);
         }
     }
 
